95a4f4abf23f9038a8e2fc25d41a546b79ecec2f
Steven Turner <steventurner2@outlook.com>
2025-09-14 01:19:04 +0100

    make endpoint plural

--- src/main/java/com/steve/callofthebacklog/MainController.java
+++ src/main/java/com/steve/callofthebacklog/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +18,7 @@ public class MainController {
     @Autowired
     BacklogMapper backlogMapper;
 
-    @GetMapping(path = "/backlog")
+    @GetMapping(path = "/backlogs")
     public ResponseEntity<Iterable<BacklogDTO>> findAllBacklogs() {
         return ResponseEntity.ok(backlogRepository.findAll()
                 .stream()
